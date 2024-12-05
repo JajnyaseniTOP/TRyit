c60b1e2a388d42a9c76d6345808647f2ee1a0afc
JajnyaseniTOP <jajnyaseni.swain@theoutsourcepro.com.au>
2024-12-05 16:20:36 +0530

    adding

---
+++ src/session1/Learn.java
@@ -0,0 +1,12 @@
+package session1;
+
+public class Learn {
+
+	public static void main(String[] args) {
+		System.out.println("hello world");
+		System.out.println("hii");
+		System.out.println("hello");
+
+	}
+
+}
